9a7de3757307f157276f126ce70aa891b8660493
Dedra-Rep <dedra.rep@gmail.com>
2026-02-06 13:40:36 -0300

    Delete API directory

--- API/health.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    status: "ok",
-    service: "Mordomo API",
-    hint: "Use POST em /api/chat com JSON { message: \"...\" }"
-  });
-}
